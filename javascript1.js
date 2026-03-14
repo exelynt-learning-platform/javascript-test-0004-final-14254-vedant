aa7088e0b4f7eaae5798686dc51585cb2e24a067
Vedant2988 <94216382+Vedant2988@users.noreply.github.com>
2026-03-14 11:35:12 +0530

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,12 @@
+let n = 4;
+
+for (let i = 0; i < 2 * n - 1; i++) {
+  let row = "";
+
+  for (let j = 0; j < 2 * n - 1; j++) {
+    let min = Math.min(i, j, 2 * n - 2 - i, 2 * n - 2 - j);
+    row += (n - min) + " ";
+  }
+
+  console.log(row);
+}
